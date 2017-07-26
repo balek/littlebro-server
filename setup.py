9cbc3410bb01e56fce6b29c617edce52cf74a892
Alexey Balekhov <a@balek.ru>
2017-07-26 17:30:13 +0300

    Version 0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'littlebro-server',
-    version = '0.3',
+    version = '0.4',
     description = 'CCTV system',
     author = 'Alexey Balekhov',
     author_email = 'a@balek.ru',
